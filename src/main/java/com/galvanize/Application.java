3709442eb3ca5c4d3549c269ff61e832623d16fb
deleonenriqueta <enriqueta.correa@gmail.com>
2022-07-12 13:09:34 -0700

    added fizzBuzz method

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,19 @@
 package com.galvanize;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    static String fizzBuzz(int x) {
+        if (x % 3 == 0 && x % 5 != 0) {
+            return "Fizz";
+        } else if (x % 5 == 0 && x % 3 != 0) {
+            return "Buzz";
+        } else if (x % 3 == 0 && x % 5 == 0) {
+            return "FizzBuzz";
+        } else {
+            return String.valueOf(x);
+        }
     }
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(Integer.parseInt(args[0])));
+    }
+
 }
